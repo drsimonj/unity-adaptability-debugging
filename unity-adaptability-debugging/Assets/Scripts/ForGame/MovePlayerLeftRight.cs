fff4a10d13c46c40355a313c6b1a50e19845c512
drsimonj <drsimonjackson@gmail.com>
2016-07-12 17:41:18 +1000

    Convert to working example of logging

---
+++ unity-adaptability-debugging/Assets/Scripts/ForGame/MovePlayerLeftRight.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePlayerLeftRight : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody rb;
+
+	// Vars for logging data.
+	private LogManager lm;
+	private string eventLogPath;
+	private string streamLogPath_horizontal = "cube_horizontal.txt";  // Note that we can create multiple stream files like this (dif names and add in start()).
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+
+		// Set up Log Manager and log file paths
+		lm = GameObject.FindGameObjectWithTag ("GameController").GetComponent<LogManager> ();
+		eventLogPath = lm.eventLogPath;
+		streamLogPath_horizontal = lm.streamLogDirectory + streamLogPath_horizontal;
+	}
+
+	// Stream log of the horizontal force applied through the keyboard
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, 0.0f);
+		rb.AddForce (movement * speed);
+
+		lm.Log (streamLogPath_horizontal, moveHorizontal);
+	}
+
+	// Event log
+	void OnCollisionEnter (Collision col) {
+		lm.Log (eventLogPath, "cube_collision", col.gameObject.name);
+	}
+}
---
+++ unity-adaptability-debugging/Assets/Scripts/LogManager.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class LogManager : MonoBehaviour {
+
+	// Session data
+	public string uid = "testUser";
+	public int GmtPlus = 10;  // The GMT + (hours) of local timezone for logging; Default = 10 for Sydney, Australia.
+	private static System.DateTime epochStart = new System.DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+
+	// File Directories and Paths
+	[HideInInspector]
+	public string logDirectory;
+	public string sessionLogPath;
+	public string eventLogPath;  // Common file for all events.
+	public string streamLogDirectory;  // Paths to stream logs are set in relevant scripts, but placed in this dedicated directory
+
+	void Awake ()
+	{
+		// Set File directories and Paths
+		logDirectory       = Application.dataPath + "/logs/" + uid + "/";  // Log directory for user in Assets/logs/
+		sessionLogPath     = logDirectory + "session.txt";
+		eventLogPath       = logDirectory + "events.txt";
+		streamLogDirectory = logDirectory + "streams/";
+
+		//check if exists/create directories
+		/*if(!Directory.Exists(logDirectory))
+		{    
+			Directory.CreateDirectory(logDirectory);
+		}*/
+
+		if(!Directory.Exists(streamLogDirectory))
+		{    
+			Directory.CreateDirectory(streamLogDirectory);
+		}
+
+		// Log session details
+		StreamWriter sw = new StreamWriter(sessionLogPath);
+		sw.WriteLine ("Session details:");
+		sw.WriteLine ( "" );
+		sw.WriteLine ("start\t" + System.DateTime.UtcNow.AddHours (GmtPlus));
+		sw.WriteLine ("GMT+\t" + GmtPlus.ToString());
+		sw.WriteLine ("uid\t" + uid);
+		sw.Close();
+	}
+
+	// For event logging
+	public void Log (string PathToLog, string eventName) {
+		Log (PathToLog, eventName, "");
+	}
+	public void Log (string PathToLog, string eventName, string additional) {
+		File.AppendAllText(PathToLog, EpochTime ().ToString() + "\t" + eventName + "\t" + additional + "\n");
+	}
+
+	// For stream logging
+	public void Log (string PathToLog, double value) {
+		File.AppendAllText(PathToLog, EpochTime ().ToString() + "\t" + value.ToString() + "\n");
+	}
+
+	// To get Epoch Time
+	public static double EpochTime () {
+		return (((System.DateTime.UtcNow - epochStart).TotalMilliseconds * 100));  // * 100 to remove decimal places
+	}
+}
