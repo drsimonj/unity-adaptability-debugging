32c543af6f058c4cdc9dd84be6c7645026b3a519
drsimonj <drsimonjackson@gmail.com>
2016-07-12 13:48:08 +1000

    Initial Commit

---
+++ unity-adaptability-debugging/Assets/Scripts/TimeStamps.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeStamps : MonoBehaviour {
+
+	private System.DateTime epochStart = new System.DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+	private double epochTime;
+
+	void Start() {
+		Debug.Log (System.DateTime.UtcNow);
+	}
+
+	void Update () {
+		epochTime = (System.DateTime.UtcNow - epochStart).TotalMilliseconds;
+		Debug.Log (epochTime);
+	}
+}
---
+++ unity-adaptability-debugging/Assets/Scripts/WriteToFile.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class WriteToFile : MonoBehaviour {
+
+	private static string uid = "testUser";
+	private static string logPath = Application.dataPath + "/logs/" + uid + "/";
+
+	void Start ()
+	{
+		//check if exists/create "logs/uid" directory in Assets folder
+		if(!Directory.Exists(logPath))
+		{    
+			Directory.CreateDirectory(logPath);
+		}
+
+		WriteFile();
+	}
+
+	public static void WriteFile()
+	{
+
+		StreamWriter sw = new StreamWriter(logPath + "table.txt" );
+
+		sw.WriteLine( "Generated table of 1 to 10" );
+		sw.WriteLine( "" );
+
+		for ( int i = 1; i <= 10; i++ )
+		{
+			for ( int j = 1; j <= 10; j++ )
+			{
+				sw.WriteLine( "{0}x{1}= {2}", i, j, (i*j) );
+			}
+
+			sw.WriteLine( "====================================" );
+		}
+
+		sw.WriteLine( "Table successfully written to file!");
+
+		sw.Close();
+	}
+}
