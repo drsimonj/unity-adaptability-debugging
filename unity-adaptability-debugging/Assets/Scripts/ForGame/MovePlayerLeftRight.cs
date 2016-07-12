960ba26020dce06836e609dc7053275ae37a857f
drsimonj <drsimonjackson@gmail.com>
2016-07-13 08:51:49 +1000

    Fix event log name; convert logs to tsv; general improvements

--- unity-adaptability-debugging/Assets/Scripts/ForGame/MovePlayerLeftRight.cs
+++ unity-adaptability-debugging/Assets/Scripts/ForGame/MovePlayerLeftRight.cs
@@ -8,31 +8,34 @@ public class MovePlayerLeftRight : MonoBehaviour {
 
 	// Vars for logging data.
 	private LogManager lm;
-	private string eventLogPath;
-	private string streamLogPath_horizontal = "cube_horizontal.txt";  // Note that we can create multiple stream files like this (dif names and add in start()).
+	private string eventLogName_collisions = "cube_collision";    // note that we can create multiple event types, just add where necessary
+	private string streamLogFile_horizontal = "cube_horizontal";  // We can create multiple stream files like this (dif names and add in start() as example below shows).
+	private string streamLogFile_vertical = "cube_vertical";
 
 	void Start ()
 	{
 		rb = GetComponent<Rigidbody>();
 
-		// Set up Log Manager and log file paths
+		// Set up Log Manager and stream log file paths
 		lm = GameObject.FindGameObjectWithTag ("GameController").GetComponent<LogManager> ();
-		eventLogPath = lm.eventLogPath;
-		streamLogPath_horizontal = lm.streamLogDirectory + streamLogPath_horizontal;
+		streamLogFile_horizontal = lm.CreateStreamPath (streamLogFile_horizontal);
+		streamLogFile_vertical = lm.CreateStreamPath (streamLogFile_vertical);
 	}
 
 	// Stream log of the horizontal force applied through the keyboard
 	void FixedUpdate ()
 	{
 		float moveHorizontal = Input.GetAxis ("Horizontal");
-		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, 0.0f);
+		float moveVertical   = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 		rb.AddForce (movement * speed);
 
-		lm.Log (streamLogPath_horizontal, moveHorizontal);
+		lm.Log (streamLogFile_horizontal, moveHorizontal);
+		lm.Log (streamLogFile_vertical,   moveVertical);
 	}
 
 	// Event log
 	void OnCollisionEnter (Collision col) {
-		lm.Log (eventLogPath, "cube_collision", col.gameObject.name);
+		lm.Log (eventLogName_collisions, col.gameObject.name);
 	}
 }
