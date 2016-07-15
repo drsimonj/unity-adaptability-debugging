66d1c159301bfe09e674f0e3de7ac8cc07b62cf2
drsimonj <drsimonjackson@gmail.com>
2016-07-15 17:11:13 +1000

    Add HelicopterLog ability

---
+++ unity-adaptability-debugging/Assets/Scripts/HelicopterReporting.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelicopterReporting : MonoBehaviour {
+
+	public GameObject LogMenu;
+	public string ShowMenuKey = "escape";
+
+	private bool isShowing = false;
+
+	void Start () {
+		LogMenu.SetActive (isShowing);
+	}
+
+	void Update () {
+		if (Input.GetKeyDown(ShowMenuKey)) {
+			isShowing = !isShowing;
+			LogMenu.SetActive(isShowing);
+		}
+	}
+}
